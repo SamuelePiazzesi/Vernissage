e884e66c0b02bd8b47f3b44127db07d00f387af7
Samuele Piazzesi <samuelepiazzesi@gmail.com>
2023-03-31 20:05:32 +0200

    add parcel

---
+++ dist/main.js
@@ -0,0 +1,18 @@
+var $8zHUo$reactjsxruntime = require("react/jsx-runtime");
+require("react");
+
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$export(module.exports, "Photo", () => $f3c52afbef1377a8$export$b3c726fd70019c38);
+
+
+const $f3c52afbef1377a8$export$b3c726fd70019c38 = ()=>{
+    return /*#__PURE__*/ (0, $8zHUo$reactjsxruntime.jsx)("img", {});
+};
+
+
+
+
+//# sourceMappingURL=main.js.map
